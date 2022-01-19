1395deb950d4d28e7fd86d0ca9a71c972b6653ba
ibaialberdi <ibaialberdi@gmail.com>
2022-01-19 11:04:13 +0100

    added basic main and error views

--- views/main/main.php
+++ views/main/main.php
@@ -1,1 +1,18 @@
 <!-- This is the main generic view of your application, it's not required to use it -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+</head>
+
+<body>
+    <h1>Uver Manager</h1>
+    <div class="list-group">
+        <a class="list-group-item list-group-item-action" href="?controller=driver&action=getAllDrivers">Driver</a>
+</body>
+
+</html>
