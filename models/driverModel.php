f3d1ced1bad974b844cf6198d222e0edd4ece45f
ibaialberdi <ibaialberdi@gmail.com>
2022-01-19 11:20:29 +0100

    added get function to driver model

--- models/driverModel.php
+++ models/driverModel.php
@@ -1,6 +1,17 @@
 <?php
 
+require_once("helper/dbConnection.php");
+
 function get(){
+    $query = conn()->prepare(("SELECT * FROM drivers"));
+
+    try {
+        $query->execute();
+        $drivers = $query->fetchAll();
+        return $drivers;
+    } catch (PDOException $e) {
+        return [];
+    }
 }
 
 function getById($id){
