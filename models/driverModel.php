4fb3ee46b97a95fa4186b53c7b60e1235f669a47
ibaialberdi <ibaialberdi@gmail.com>
2022-01-19 18:21:51 +0100

    added getDriverById to driverModel

--- models/driverModel.php
+++ models/driverModel.php
@@ -15,4 +15,14 @@ function get(){
 }
 
 function getById($id){
+    $query = conn()->prepare("SELECT * FROM drivers WHERE id = :id");
+    $query->bindParam(":id", $id);
+
+    try {
+        $query->execute();
+        $drivers = $query->fetchAll();
+        return $drivers;
+    } catch (PDOException $e) {
+        return [];
+    }
 }
