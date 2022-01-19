1212ae0531dd29e42da7b3c247c2ede3e4c5cc47
ibaialberdi <ibaialberdi@gmail.com>
2022-01-19 10:59:14 +0100

    added basic logic to index.php

--- index.php
+++ index.php
@@ -5,6 +5,25 @@
 // dynamically so that it ends up including the view.
 
 include_once "config/constants.php";
+include_once "config/db.php";
+
+if(isset($_GET['controller'])) {
+    $controller = getControllerPath($_GET['controller']);
+    $fileExists = file_exists($controller);
+    if ($fileExists) {
+        require_once $controller;
+    } else {
+        $errorMsg = "The you are trying to access does not exist.";
+        require_once VIEWS . "error/error.php";
+    }
+} else {
+    require_once VIEWS . "main/main.php";
+}
+
+function getControllerPath($controller): string
+{
+    return CONTROLLERS . $controller . "Controller.php";
+}
 
 // TODO Implement the logic to include the controller passed by the URL dynamically
 // In the event that the controller passed by URL does not exist, you must show the error view.
